8943adc66db33500f52949dbd43916d42c089b6e
Nathan Brown <scottynate05@yahoo.com>
2020-11-10 19:13:55 -0800

    obj2 started

---
+++ src/obj2.py
@@ -0,0 +1,14 @@
+# Describe the differences between in-place and out-of-place algorithms
+
+x = []
+n = [1, 2, 3]
+def tripple_in_place(nums):
+    for index in range(len(nums)):
+        # nums[index] = nums[index] * 3
+        nums[index] *= 3
+
+print(n)
+x = tripple_in_place(n)
+print(n)
+
+print(x)
